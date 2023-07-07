e06f9da5826324e699b2a4f511be43c18299c857
barakthegreat <115320962+barakthegreat@users.noreply.github.com>
2023-07-07 17:20:40 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
-#include“OTPGenerator.h” //Umoh Ephraim Gabriel 19/097344156
-#include“PhoneNumberInput.h” //Ahilaga Barak 19/097344008
-#include“OTPSender.h” //Ekele Emmanuel Anyanwu 19/097344050
-#include“OTPVerifier.h” //Ekom-Nta Mary 19/097344051
-#include“AccessController.h” //Effem Holy Cyprian 19/097344044
+#include <iostream>
+#include "OTPGenerator.h" //Umoh Ephraim Gabriel 19/097344156
+#include "PhoneNumberInput.h" //Ahilaga Barak 19/097344008
+#include "OTPSender.h" //Ekele Emmanuel Anyanwu 19/097344050
+#include "OTPVerifier.h" //Ekom-Nta Mary 19/097344051
+#include"AccessController.h" //Effem Holy Cyprian 19/097344044
 Using namespace std;
 
 int main()
 {
     std::string phoneNumber;
     int otp;
-    Bool isVerified = false;
+    bool isVerified = false;
 
     phoneNumber = getPhoneNumberFromUser();
     otp = generateOTP();
@@ -19,7 +19,7 @@ int main()
     while (!isVerified)
     {
         int userOTP;
-        std::cout << “Enter the OTP: “;
+        std::cout << "Enter the OTP:";
         std::cin >> userOTP;
 
         if (verifyOTP(userOTP, otp))
@@ -33,5 +33,5 @@ int main()
         }
     }
 
-    Return 0;
+    return 0;
 }
