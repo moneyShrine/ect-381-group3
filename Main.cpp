a121f6e7d1b428576ce80b2d97351887cc4085a1
barakthegreat <115320962+barakthegreat@users.noreply.github.com>
2023-07-07 03:58:50 +0100

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#Include “OTPGenerator.h” //Umoh Ephraim Gabriel 19/097344156
+#include “OTPGenerator.h” //Umoh Ephraim Gabriel 19/097344156
 #include “PhoneNumberInput.h” //Ahilaga Barak 19/097344008
 #include “OTPSender.h” //Ekele Emmanuel Anyanwu 19/097344050
 #include “OTPVerifier.h” //Ekom-Nta Mary 19/097344051
