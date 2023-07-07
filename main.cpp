92ef4e8fcefc21adccc34390b767654b95339ff0
barakthegreat <115320962+barakthegreat@users.noreply.github.com>
2023-07-07 16:57:01 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
-#include “OTPGenerator.h” //Umoh Ephraim Gabriel 19/097344156
-#include “PhoneNumberInput.h” //Ahilaga Barak 19/097344008
-#include “OTPSender.h” //Ekele Emmanuel Anyanwu 19/097344050
-#include “OTPVerifier.h” //Ekom-Nta Mary 19/097344051
-#include “AccessController.h” //Effem Holy Cyprian 19/097344044
+#include“OTPGenerator.h” //Umoh Ephraim Gabriel 19/097344156
+#include“PhoneNumberInput.h” //Ahilaga Barak 19/097344008
+#include“OTPSender.h” //Ekele Emmanuel Anyanwu 19/097344050
+#include“OTPVerifier.h” //Ekom-Nta Mary 19/097344051
+#include“AccessController.h” //Effem Holy Cyprian 19/097344044
 Using namespace std;
 
-Int main()
+int main()
 {
-    Std::string phoneNumber;
-    Int otp;
+    std::string phoneNumber;
+    int otp;
     Bool isVerified = false;
 
     phoneNumber = getPhoneNumberFromUser();
@@ -18,16 +18,16 @@ Int main()
 
     while (!isVerified)
     {
-        Int userOTP;
-        Std::cout << “Enter the OTP: “;
-        Std::cin >> userOTP;
+        int userOTP;
+        std::cout << “Enter the OTP: “;
+        std::cin >> userOTP;
 
-        If (verifyOTP(userOTP, otp))
+        if (verifyOTP(userOTP, otp))
         {
             grantAccess();
             isVerified = true;
         }
-        Else
+        else
         {
             blockAccess();
         }
