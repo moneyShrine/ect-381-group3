89c9cbff4b1953d1b26d8a0a39405001f2d5a315
barakthegreat <115320962+barakthegreat@users.noreply.github.com>
2023-07-26 20:52:10 +0100

    Create otpgenerator.h

---
+++ otpgenerator.h
@@ -0,0 +1,13 @@
+//written by
+//Ahilaga Barak
+#ifndef OTP_GENERATOR_H
+#define OTP_GENERATOR_H
+
+#include <string>
+
+class OTPGenerator {
+public:
+    static std::string generateOTP(int length);
+};
+
+#endif // OTP_GENERATOR_H
