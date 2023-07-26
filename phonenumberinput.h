e5b5a9cd326aac8fefc8173d924a4881fa33e53f
barakthegreat <115320962+barakthegreat@users.noreply.github.com>
2023-07-26 20:53:20 +0100

    Create phonenumberinput.h

---
+++ phonenumberinput.h
@@ -0,0 +1,14 @@
+//written by
+//Ekom-nta Mary
+//19/097344051
+#ifndef PHONE_NUMBER_INPUT_H
+#define PHONE_NUMBER_INPUT_H
+
+#include <string>
+
+class PhoneNumberInput {
+public:
+    static std::string getPhoneNumberFromUser();
+};
+
+#endif // PHONE_NUMBER_INPUT_H
