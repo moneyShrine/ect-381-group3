6e529337e11ab2549985e27c0e283fc150f43e1b
barakthegreat <115320962+barakthegreat@users.noreply.github.com>
2023-07-26 20:55:16 +0100

    Create otpsender.h

---
+++ otpsender.h
@@ -0,0 +1,15 @@
+/written by
+//Ekele Emmanuel Anyanwu
+//19/097344050
+
+#ifndef OTP_SENDER_H
+#define OTP_SENDER_H
+
+#include <string>
+
+class OTPSender {
+public:
+    static bool sendOTP(const std::string& phoneNumber, const std::string& otp);
+};
+
+#endif // OTP_SENDER_H
